3cf63d6a1948ba29605a0bcc5bb8e13cb04e7c03
Olivier Berger <olivier.berger@telecom-sudparis.eu>
2018-08-07 15:00:45 +0200

    End product of TP 3

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -20,98 +20,38 @@ use Symfony\Component\Routing\Annotation\Route;
 class TodoController extends Controller
 {    
     /**
-     * @Route("/", name = "home", methods="GET")
-     */
-    public function indexAction()
-    {
-        $htmlpage = '<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Welcome!</title>
-    </head>
-    <body>
-        <h1>Welcome</h1>
-            
-    <p>Bienvenue dans notre todo list</p>
-    </body>
-</html>';
-        
-        return new Response(
-            $htmlpage,
-            Response::HTTP_OK,
-            array('content-type' => 'text/html')
-            );
-    }
-    /**
      * Lists all todo entities.
-     *
+     * @Route("/", name = "todo_home", methods="GET")
      * @Route("/list", name = "todo_list", methods="GET")
      * @Route("/index", name="todo_index", methods="GET")
      */
     public function listAction()
     {
-        $htmlpage = '<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>todos list!</title>
-    </head>
-    <body>
-        <h1>todos list</h1>
-        <p>Here are all your todos:</p>
-        <ul>';
-        
         $em = $this->getDoctrine()->getManager();
+        
         $todos = $em->getRepository(Todo::class)->findAll();
-        foreach($todos as $todo) {
-           $htmlpage .= '<li>
-            <a href="/todo/'.$todo->getid().'">'.$todo->getTitle().'</a></li>';
-         }
-        $htmlpage .= '</ul>';
-
-        return new Response(
-            $htmlpage,
-            Response::HTTP_OK,
-            array('content-type' => 'text/html')
-            );
+        
+        return $this->render('todo/index.html.twig', array(
+            'todos' => $todos,
+        ));
     }
-
     /**
      * Lists all active todo entities.
-     * 
+     *
      * The todo entities which aren't yet completed
      *
      * @Route("/list-active", name = "todo_active_list", methods="GET")
      */
     public function activelistAction()
     {
-        $htmlpage = '<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>todos list!</title>
-    </head>
-    <body>
-        <h1>todos list</h1>
-        <p>Here are all active todos:</p>
-        <ul>';
-        
         $em = $this->getDoctrine()->getManager();
-        $todos = $em->getRepository(Todo::class)->findByCompleted(false);
-        foreach($todos as $todo) {
-            $htmlpage .= '<li>
-            <a href="/todo/'.$todo->getid().'">'.$todo->getTitle().'</a></li>';
-        }
-        $htmlpage .= '</ul>';
         
-        return new Response(
-            $htmlpage,
-            Response::HTTP_OK,
-            array('content-type' => 'text/html')
-            );
+        $todos = $em->getRepository(Todo::class)->findByCompleted(false);
+        
+        return $this->render('todo/index.html.twig', array(
+            'todos' => $todos,
+        ));
     }
-    
     /**
      * Finds and displays a todo entity.
      *
@@ -119,27 +59,8 @@ class TodoController extends Controller
      */
     public function showAction(Todo $todo): Response
     {
-        $htmlpage = '<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>todo n° '.$todo->getId().' details</title>
-    </head>
-    <body>
-        <h2>Todo Details :</h2>
-        <ul>
-        <dl>';
-        
-        $htmlpage .= '<dt>TODO</dt><dd>' . $todo->getTitle() . '</dd>';
-        $htmlpage .= '<dt>Date de création</dt> <dd> ' . $todo->getCreated()->format('Y-m-d') . '</dd>';
-        $htmlpage .= '<dt>Date de modification</dt> <dd> '. $todo->getUpdated()->format('Y-m-d') . '</dd>';
-        $htmlpage .= '<dt>Satus</dt><dd>' . $todo->getCompleted() ? '	Terminé ' : ' En cours ' . '</dd>';
-        $htmlpage .= '</dl>';
-        
-        return new Response(
-                $htmlpage,
-                Response::HTTP_OK,
-                array('content-type' => 'text/html')
-                );
+        return $this->render('todo/show.html.twig', array(
+            'todo' => $todo,
+        ));
     }
 }
