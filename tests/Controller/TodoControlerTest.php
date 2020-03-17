ce322d68d7f9ddd0a312615eb97952a9be670533
Christian Bac <christian.bac@telecom-sudparis.eu>
2020-03-17 09:17:24 +0100

    Ajout test lien entre listes todo et todo et backling

--- tests/Controller/TodoControlerTest.php
+++ tests/Controller/TodoControlerTest.php
@@ -16,7 +16,96 @@ class TodoControllerTest extends WebTestCase
         $client->request('GET', $url);
         $this->assertTrue($client->getResponse()->isSuccessful());
     }
+    public function testListContainsTable()
+    {
+        $client = self::createClient();
+        $crawler = $client->request('GET', '/todo/list');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html table')->count()
+            );
+    }
+    public function testListTableContainsLink()
+    {
+        $client = self::createClient();
+        $crawler = $client->request('GET', '/todo/list');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html a')->count()
+            );
+    }
+    public function testClickOnFirstTodo()
+    {
+        $client = self::createClient();
+        $crawler = $client->request('GET', '/todo/list');
+        $link = $crawler
+        ->filter('a:contains("show")') // find all links with the text "Greet"
+        ->eq(0) // select the second link in the list
+        ->link()
+        ;
+        
+        // and click it
+        $crawler = $client->click($link);
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        
+    }
 
+    public function testFirstTodoContainsBackLink()
+    {
+        $client = self::createClient();
+        $crawler = $client->request('GET', '/todo/list');
+        $link = $crawler
+        ->filter('a:contains("show")') // find all links with the text "show"
+        ->eq(0) // select the first link in the list
+        ->link()
+        ;
+        
+        // and click it
+        $crawler = $client->click($link);
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('a:contains("back")')->count()
+            );
+        
+    }
+    public function testListActiveContainsTable()
+    {
+        $client = self::createClient();
+        $crawler = $client->request('GET', '/todo/list-active');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html table')->count()
+            );
+    }
+    public function testListTableActiveContainsLink()
+    {
+        $client = self::createClient();
+        $crawler = $client->request('GET', '/todo/list-active');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html a')->count()
+            );
+    }
+    public function testClickOnFirstActiveTodo()
+    {
+        $client = self::createClient();
+        $crawler = $client->request('GET', '/todo/list-active');
+        $link = $crawler
+        ->filter('a:contains("show")') // find all links with the text "show"
+        ->eq(0) // select the first link in the list
+        ->link()
+        ;
+        
+        // and click it
+        $crawler = $client->click($link);
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        
+    }
     public function urlProvider()
     {
         yield ['/todo/'];
