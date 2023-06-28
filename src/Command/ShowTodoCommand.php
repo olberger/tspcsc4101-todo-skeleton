3ba7d24c0b640e2802534b71b9ad180146708e24
Olivier Berger <olivier.berger@telecom-sudparis.eu>
2023-06-28 21:21:36 +0200

    Update to Symfony 6.3, more

--- src/Command/ShowTodoCommand.php
+++ src/Command/ShowTodoCommand.php
@@ -15,7 +15,6 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Doctrine\Persistence\ManagerRegistry;
@@ -30,7 +29,7 @@ use Doctrine\Persistence\ManagerRegistry;
     name: 'app:show-todo',
     description: 'Show one todo',
     )]
- class ShowTodoCommand extends Command
+class ShowTodoCommand extends Command
     {
         
         /**
