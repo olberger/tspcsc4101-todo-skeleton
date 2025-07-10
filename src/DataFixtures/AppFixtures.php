bea837b808599844f7e87fb933a5102500c43287
Olivier Berger <olivier.berger@telecom-sudparis.eu>
2025-07-10 15:35:41 +0200

    Align interface to avoid PHP complaining

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -17,7 +17,7 @@ use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
         $this->loadTodos($manager);
     }
